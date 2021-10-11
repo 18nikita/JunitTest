9d287dda44367f0925db15d0475791c73a72ea52
unknown <rajputnikita5666@gmail.com>
2021-10-11 17:54:11 +0530

    Added:password rule for numeric value

--- src/main/java/com/bridgelabz/junit/UserValidate.java
+++ src/main/java/com/bridgelabz/junit/UserValidate.java
@@ -10,8 +10,9 @@ public class UserValidate {
 	private final String EMAIL_VALIDATOR = "^([a][b][c])([\\.][a-z]+)[@][b][l][\\.][c][o]([\\.][i][n])";
 	private final String MOBILE_NUM_VALIDATOR = "(0/91)?[7-9][0-9]{9}";
 	private final String PASSWORD_RULE1_VALIDATOR = "[a-z]{8,}";
-	private final String PASSWORD_RULE2_VALIDATOR = "[A-z]{1,}[a-z]{8,}";
-	
+	private final String PASSWORD_RULE2_VALIDATOR = "[A-Z]{1,}[a-z]{8,}";
+	private final String PASSWORD_RULE3_VALIDATOR = "[a-z A-z 0-9]{8,}";
+
 	static Scanner sc = new Scanner(System.in);
 
 	public static boolean firstNameValidator() {
@@ -20,7 +21,6 @@ public class UserValidate {
 		String fName = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.NAME_VALIDATOR, fName);
 	}
-	
 
 	public static boolean lastNameValidator() {
 		UserValidate uservalidate = new UserValidate();
@@ -28,35 +28,40 @@ public class UserValidate {
 		String lName = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.NAME_VALIDATOR, lName);
 	}
-	
+
 	public static boolean emailValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Email address: ");
 		String email = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.EMAIL_VALIDATOR, email);
 	}
-	
+
 	public static boolean mobNumValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Mobile number: ");
 		String mobNum = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.MOBILE_NUM_VALIDATOR, mobNum);
 	}
-	
+
 	public static boolean password1Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password: ");
 		String password1 = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE1_VALIDATOR, password1);
 	}
-	
+
 	public static boolean password2Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password 2: ");
 		String password2 = uservalidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE2_VALIDATOR, password2);
 	}
-
-
 	
+	public static boolean password3Validator() {
+		UserValidate uservalidate = new UserValidate();
+		System.out.println("Enter the Password 3: ");
+		String password3 = uservalidate.sc.nextLine();
+		return Pattern.matches(uservalidate.PASSWORD_RULE3_VALIDATOR, password3);
+	}
+
 }
