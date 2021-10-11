b1bc4964518d0db9a5000f2bd8f840dee0a77e55
unknown <rajputnikita5666@gmail.com>
2021-10-11 16:46:52 +0530

    Added:Password rule for charactor

--- src/test/java/com/bridgelabz/junit/UserValidateTest.java
+++ src/test/java/com/bridgelabz/junit/UserValidateTest.java
@@ -22,6 +22,6 @@ public class UserValidateTest {
 
 	@Test
 	public void mobNumTestValidator() {
-		assertEquals(true, UserValidate.mobNumValidator());
+		assertEquals(true, UserValidate.password1Validator());
 	}
 }
