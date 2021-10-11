8639f3f224c10b26bc13d993e9e11e709a1f6987
unknown <rajputnikita5666@gmail.com>
2021-10-11 15:26:17 +0530

    Added:First name validation

---
+++ src/test/java/com/bridgelabz/junit/UserValidateTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UserValidateTest {
+	@Test
+	public void firstNameTestValidator() {
+		assertEquals(true, UserValidate.firstNameValidator());
+	}
+}
