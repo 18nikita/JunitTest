fb56a96fbc2393017e235f29bf582a9cc76f9bd0
unknown <rajputnikita5666@gmail.com>
2021-10-20 23:37:07 +0530

    Added:Analyse mood for test cases

--- src/test/java/com/bridgelabz/junit/UserValidateTest.java
+++ src/test/java/com/bridgelabz/junit/UserValidateTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class UserValidateTest {
 	@Test
-	public void firstNameTestValidator() {
+	public void givenFirstNameShouldReturnTrue() {
 		assertEquals(true, UserValidate.firstNameValidator());
 	}
 
@@ -24,19 +24,31 @@ public class UserValidateTest {
 	public void password1Validator() {
 		assertEquals(true, UserValidate.password1Validator());
 	}
-	
+
 	@Test
 	public void password2Validator() {
 		assertEquals(true, UserValidate.password2Validator());
 	}
-	
+
 	@Test
 	public void password3Validator() {
 		assertEquals(true, UserValidate.password3Validator());
 	}
-	
+
 	@Test
 	public void password4Validator() {
 		assertEquals(true, UserValidate.password4Validator());
+	}
+
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnEntrySuccessful() {
+		String result = MoodAnalyser.analyseMood("User is Happy");
+		assertEquals("Entry Successful", result);
+	}
+
+	@Test
+	public void givenEmail2_WhenNotProper_ShouldReturnEntryFailed() {
+		String result = MoodAnalyser.analyseMood("User is Sad");
+		assertEquals("Entry Failed", result);
 	}
 }
